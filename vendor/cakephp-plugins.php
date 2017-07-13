f0ad4faca9b383f520c2c2f737aff7879bddb3a2
Filipe Morelli <morellitecinfo@gmail.com>
2017-07-13 17:25:53 -0300

    Arquivos necessarios para testar em qualquer maquina

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,10 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'Crawler' => $baseDir . '/vendor/patarkf/crawler-cakephp/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
+    ]
+];
